85caaab3aca90a7c177058556bbe5aac2c636142
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-14 17:21:12 +0530

    commit all changes successfully

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        qrbg:"#E8F9FD"
+      },
+      fontFamily:{
+        cardbody: ["Nunito"]
+      }
+    },
+  },
+  plugins: [],
+}
